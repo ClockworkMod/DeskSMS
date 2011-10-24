95477a40aa5fdb1b3ef095f092007ba2b6d99eff
Joseph Bass <jab416171@gmail.com>
2011-10-24 16:22:22 -0600

    Added Blacklist feature to DeskSMS

--- DeskSMS/gen/com/clockworkmod/billing/R.java
+++ DeskSMS/gen/com/clockworkmod/billing/R.java
@@ -98,28 +98,30 @@ public final class R {
         public static final int twofortyfouram_locale_help_save_dontsave=0x7f080000;
     }
     public static final class string {
+        public static final int DeskSMS_blacklist_title=0x7f05002a;
         public static final int account=0x7f05001b;
-        public static final int account_status=0x7f050037;
+        public static final int account_status=0x7f05003a;
         public static final int accounts=0x7f050020;
-        public static final int add_desksms_contact_info=0x7f050029;
-        public static final int add_desksms_contact_info_summary=0x7f05002a;
-        public static final int adjust_hour=0x7f050043;
-        public static final int adjust_hours=0x7f050044;
-        public static final int adjust_sms_date=0x7f050041;
-        public static final int adjust_sms_date_summary=0x7f050042;
-        public static final int android_market_inapp=0x7f050035;
+        public static final int add_desksms_contact_info=0x7f05002c;
+        public static final int add_desksms_contact_info_summary=0x7f05002d;
+        public static final int adjust_hour=0x7f050046;
+        public static final int adjust_hours=0x7f050047;
+        public static final int adjust_sms_date=0x7f050044;
+        public static final int adjust_sms_date_summary=0x7f050045;
+        public static final int android_market_inapp=0x7f050038;
         public static final int app_name=0x7f050001;
-        public static final int authentification_notification=0x7f050047;
-        public static final int buy_desksms=0x7f050032;
+        public static final int authentification_notification=0x7f05004a;
+        public static final int blacklist_remove_number=0x7f05002b;
+        public static final int buy_desksms=0x7f050035;
         public static final int contacts=0x7f050028;
         public static final int current_account=0x7f05000d;
-        public static final int days_left=0x7f050038;
+        public static final int days_left=0x7f05003b;
         public static final int description=0x7f050002;
-        public static final int desksms_one_year_license=0x7f05003a;
+        public static final int desksms_one_year_license=0x7f05003d;
         public static final int disable_notifications=0x7f050024;
         public static final int disable_notifications_summary=0x7f050025;
-        public static final int disable_notifications_summary2=0x7f050040;
-        public static final int disabled=0x7f050045;
+        public static final int disable_notifications_summary2=0x7f050043;
+        public static final int disabled=0x7f050048;
         public static final int forwarded=0x7f05000e;
         public static final int gmail=0x7f05001e;
         public static final int google_talk=0x7f05001d;
@@ -127,78 +129,79 @@ public final class R {
         public static final int handcent=0x7f050016;
         public static final int hello=0x7f050000;
         public static final int login_with_google=0x7f050005;
+        public static final int manage_blacklist=0x7f050029;
         public static final int missed_call_at=0x7f050019;
         public static final int missed_call_from=0x7f050018;
-        public static final int mms_received=0x7f05003d;
+        public static final int mms_received=0x7f050040;
         public static final int msg_app_detected=0x7f050015;
         public static final int not_logged_in=0x7f050010;
         public static final int notifications=0x7f05001c;
         public static final int number_retrieval_error=0x7f050014;
-        public static final int payment_options=0x7f050036;
-        public static final int paypal=0x7f050034;
+        public static final int payment_options=0x7f050039;
+        public static final int paypal=0x7f050037;
         public static final int permission_request=0x7f050007;
-        public static final int please_wait_contact_list=0x7f05002c;
-        public static final int plugin_name=0x7f05003f;
+        public static final int please_wait_contact_list=0x7f05002f;
+        public static final int plugin_name=0x7f050042;
         public static final int proxied=0x7f05000f;
-        public static final int purchase_thanks=0x7f05003b;
-        public static final int push_failed=0x7f050031;
-        public static final int redeem_code=0x7f05003e;
+        public static final int purchase_thanks=0x7f05003e;
+        public static final int push_failed=0x7f050034;
+        public static final int redeem_code=0x7f050041;
         public static final int registering_with_server=0x7f05000c;
-        public static final int remove_desksms_contact_info=0x7f05002b;
+        public static final int remove_desksms_contact_info=0x7f05002e;
         public static final int requesting_permissions=0x7f050006;
         public static final int retrieving_authentication=0x7f05000b;
-        public static final int retrieving_status=0x7f050033;
+        public static final int retrieving_status=0x7f050036;
         public static final int send_email=0x7f050009;
         public static final int send_xmpp=0x7f050008;
         public static final int setting_up_push=0x7f05000a;
-        public static final int signin_complete=0x7f050046;
+        public static final int signin_complete=0x7f050049;
         public static final int signin_failure=0x7f050004;
         public static final int signin_success=0x7f050003;
         public static final int sms_received=0x7f05001a;
-        public static final int status_error=0x7f050039;
-        public static final int switch_account=0x7f05003c;
-        public static final int sync_calls=0x7f05002d;
-        public static final int sync_calls_summary=0x7f05002e;
+        public static final int status_error=0x7f05003c;
+        public static final int switch_account=0x7f05003f;
+        public static final int sync_calls=0x7f050030;
+        public static final int sync_calls_summary=0x7f050031;
         public static final int sync_sms=0x7f050026;
         public static final int sync_sms_summary=0x7f050027;
         public static final int test_message=0x7f050011;
         public static final int test_message_content=0x7f050013;
         public static final int test_message_info=0x7f050012;
-        public static final int testing_push=0x7f050030;
+        public static final int testing_push=0x7f050033;
         public static final int troubleshooting=0x7f05001f;
         /**  A toast message that appears if an application that is being launched isn't available.  There is no character length maximum, although this message will only appear on the screen for about 5 seconds in a Toast message so it should be short enough to be comprehended before it disappears 
          */
-        public static final int twofortyfouram_locale_application_not_available=0x7f05004b;
+        public static final int twofortyfouram_locale_application_not_available=0x7f05004e;
         /**  A format string for breadcrumbs.  For left to right languages, such as English, the format will look something like "Main Activity > Sub Activity > Sub Activity 2".  Item #1 is the previous path, item #2 is the separator, and item #3 is the current item   
          */
-        public static final int twofortyfouram_locale_breadcrumb_format=0x7f05004c;
+        public static final int twofortyfouram_locale_breadcrumb_format=0x7f05004f;
         /**  The separator character for breadcrumbs 
          */
-        public static final int twofortyfouram_locale_breadcrumb_separator=0x7f05004d;
+        public static final int twofortyfouram_locale_breadcrumb_separator=0x7f050050;
         /**  The button that performs a search for the app on the Market or on <www.twofortyfouram.com>.  MDPI 17 character maximum 
          */
-        public static final int twofortyfouram_locale_marketactivity_dialog_button_install=0x7f05004f;
+        public static final int twofortyfouram_locale_marketactivity_dialog_button_install=0x7f050052;
         /**  The primary message of the MarketActivity dialog.  There is no character length maximum 
          */
-        public static final int twofortyfouram_locale_marketactivity_dialog_primary_message=0x7f050050;
+        public static final int twofortyfouram_locale_marketactivity_dialog_primary_message=0x7f050053;
         /**  The secondary message of the MarketActivity dialog.  The word "Install" should match the twofortyfouram_locale_marketactivity_dialog_button_install string.  There is no character length maximum 
          */
-        public static final int twofortyfouram_locale_marketactivity_dialog_secondary_message=0x7f050051;
+        public static final int twofortyfouram_locale_marketactivity_dialog_secondary_message=0x7f050054;
         /**  The title of the MarketActivity.  MDPI 21 character maximum 
          */
-        public static final int twofortyfouram_locale_marketactivity_dialog_title=0x7f05004e;
+        public static final int twofortyfouram_locale_marketactivity_dialog_title=0x7f050051;
         /**  The Don't Save button in the menu.  MDPI 21 character maximum 
          */
-        public static final int twofortyfouram_locale_menu_dontsave=0x7f050049;
+        public static final int twofortyfouram_locale_menu_dontsave=0x7f05004c;
         /**  The Help button in the menu.  MDPI 21 character maximum 
          */
-        public static final int twofortyfouram_locale_menu_help=0x7f05004a;
+        public static final int twofortyfouram_locale_menu_help=0x7f05004d;
         /**  The Save button in the menu.  MDPI 21 character maximum 
          */
-        public static final int twofortyfouram_locale_menu_save=0x7f050048;
+        public static final int twofortyfouram_locale_menu_save=0x7f05004b;
         public static final int updating_settings=0x7f050022;
         public static final int updating_settings_error=0x7f050023;
-        public static final int web=0x7f05002f;
+        public static final int web=0x7f050032;
         public static final int widget_name=0x7f050021;
     }
     public static final class style {
